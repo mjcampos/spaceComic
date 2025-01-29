60992fdcedc7ba715a51b56076cac76698eb0435
jvaikun <jeevan.vaikun@gmail.com>
2025-01-29 20:06:30 +0700

    Starting page code refactor

--- pages/page.gd
+++ pages/page.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const KEYWORDS = ["half", "back", "end"]
+
 @onready var page_elements : Array = []
 var panel_index : Vector2 = Vector2.ZERO
 var page_finished : bool = false
@@ -11,29 +13,31 @@ signal page_ended
 
 func _ready() -> void:
 	for path in page_elements:
-        for panel in path:
-            if !(panel is String):
-                panel.modulate = Color(1, 1, 1, 0)
-            if panel is Control:
-                panel.decision_confirmed.connect(_on_decision_confirmed)
+		for element in path:
+			if !(element in KEYWORDS):
+				var this_element = get_node(element)
+				this_element.modulate = Color(1, 1, 1, 0)
+				if this_element is Control:
+					this_element.decision_confirmed.connect(_on_decision_confirmed)
 
 
 func next_panel():
-    var this_element = page_elements[panel_index.x][panel_index.y]
-	if this_element is String:
-        match this_element:
-            "half", "back":
-                print("Panning camera...")
-                cam_pan_triggered.emit(this_element)
-                panel_index.y += 1
-            "end":
-                page_finished = true
-                page_ended.emit()
+	var this_element = page_elements[panel_index.x][panel_index.y]
+	if this_element in KEYWORDS:
+		match this_element:
+			"half", "back":
+				print("Panning camera...")
+				cam_pan_triggered.emit(this_element)
+				panel_index.y += 1
+			"end":
+				page_finished = true
+				page_ended.emit()
 	else:
 		paused = true
-		print("Showing panel: %s" % this_element.name)
+		var this_node = get_node(this_element)
+		print("Showing panel: %s" % this_node.name)
 		var panel_tween = create_tween()
-		panel_tween.tween_property(this_element, "modulate", Color(1,1,1,1), 0.5)
+		panel_tween.tween_property(this_node, "modulate", Color(1,1,1,1), 0.5)
 		panel_tween.tween_callback(_on_tween_finished)
 		panel_tween.play()
 
@@ -45,6 +49,6 @@ func _on_tween_finished():
 
 func _on_decision_confirmed():
 	paused = false
-    panel_index.x = $Decision.word_index + 1
-    panel_index.y = 0
+	panel_index.x = $Decision.word_index + 1
+	panel_index.y = 0
 
